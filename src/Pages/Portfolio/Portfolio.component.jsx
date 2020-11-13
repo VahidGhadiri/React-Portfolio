b123d1f1e1b0df8628ad170af7ba7a54be96f861
VahidGhadiri <whydrf@gmail.com>
2020-11-13 14:14:30 +0330

    the portfolio page has been added

--- src/Pages/Portfolio/Portfolio.component.jsx
+++ src/Pages/Portfolio/Portfolio.component.jsx
@@ -1,33 +1,119 @@
 import React, { useState } from "react";
+import resumeData from "../../Utility/resumeData.component";
 import "./Portfolio.styles.css";
-import { Grid, Tabs } from "@material-ui/core";
+
+import {
+  Grid,
+  Tabs,
+  Tab,
+  Card,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  Typography,
+  Grow,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+} from "@material-ui/core";
 
 const Portfolio = () => {
   const [tabValue, setTabValue] = useState("All");
+  const [projectDialog, setProjectDialog] = useState(false);
+
 
   return (
-    <div>
-      <Grid container className="section pb-45 pt-45">
-        {/*title*/}
-        <Grid item className="section-title mb-30">
-          <span></span>
-          <h6 className="section-title-text">Portfolio</h6>
-        </Grid>
-
-        {/*tabs*/}
-        <Grid item>
-          <Tabs
-            value={tabValue}
-            indicatorColor="white"
-            className="customTabs"
-            onChange={(event, newValue) => {
-              setTabValue(newValue);
-            }}
-          ></Tabs>
-        </Grid>
+    <Grid container className="section pb-45 pt-45">
+      {/*title*/}
+      <Grid item className="section-title mb-20">
+        <span></span>
+        <h6 className="section-title-text">Portfolio</h6>
       </Grid>
-    </div>
+
+      {/*tabs*/}
+      <Grid item xs={12}>
+        <Tabs
+          value={tabValue}
+          indicatorColor="white"
+          className="custom-tabs"
+          onChange={(event, newValue) => {
+            setTabValue(newValue);
+          }}
+        >
+          <Tab
+            label="All"
+            value="All"
+            className={
+              tabValue == "All" ? "custom-tabs-item active" : "custom-tabs-item"
+            }
+          />
+
+          {[...new Set(resumeData.projects.map((item) => item.tag))].map(
+            (tag) => {
+              return (
+                <Tab
+                  label={tag}
+                  value={tag}
+                  className={
+                    tabValue == tag  ? "custom-tabs-item active" : "custom-tabs-item"
+                  }
+                />
+              );
+            }
+          )}
+        </Tabs>
+      </Grid>
+      {/*Projects */}
+      <Grid item xs={12}>
+        <Grid container spacing={3}>
+          {resumeData.projects.map((project) => {
+            return (
+              <>
+                {tabValue == project.tag || tabValue == "All" ? (
+                  <Grid item xs={12} sm={6} md={4}>
+                    <Grow in timeOut={1000}>
+                      <Card
+                        className="custom-card"
+                        onClick={() => setProjectDialog(project)}
+                      >
+                        <CardActionArea>
+                          <CardMedia
+                            className="custom-card-image"
+                            image={project.image}
+                            title={project.title}
+                          />
+                          <CardContent>
+                            <Typography variant={"body2"} className="custom-card-title">
+                              {project.title}
+                            </Typography>
+                            <Typography
+                              variant={"body2"}
+                              className="custom-card-caption"
+                            >
+                              {project.caption}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Grow>
+                  </Grid>
+                ) : null}
+              </>
+            );
+          })}
+        </Grid>
+        <Dialog className = "project-dialog" open={projectDialog} onClose={() => setProjectDialog(false)}>
+          <DialogTitle onClose={() => setProjectDialog(false)}>
+            {projectDialog.title}
+          </DialogTitle>
+          <img src={projectDialog.image} alt="" className="project-dialog-image" />
+          <DialogContent>
+            <Typography className="project-dialog-description">{projectDialog.description}</Typography>
+          </DialogContent>
+        </Dialog>
+      </Grid>
+    </Grid>
   );
-};
+}; 
 
 export default Portfolio;
--- src/Utility/resumeData.component.jsx
+++ src/Utility/resumeData.component.jsx
@@ -1,97 +1,145 @@
-import React from "react"
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import InstagramIcon from '@material-ui/icons/Instagram';
-import AccountTreeIcon from '@material-ui/icons/AccountTree';
-import PaletteIcon from '@material-ui/icons/Palette';
-
-
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import AccountTreeIcon from "@material-ui/icons/AccountTree";
+import PaletteIcon from "@material-ui/icons/Palette";
 
 export default {
-    name: "Vahid  Ghadiri",
-    title: "Front-end developer", 
-    phone: "0933-6891141",
-    email: "whydrf@gmail.com",
-    address: "Iran, Tehran, Ekbatan Town",
+  name: "Vahid  Ghadiri",
+  title: "Front-end developer",
+  phone: "0933-6891141",
+  email: "whydrf@gmail.com",
+  address: "Iran, Tehran, Ekbatan Town",
 
-    socials: {
-            LinkedIn: {
-                link: "https://www.linkedin.com/in/vahidghadiri/",
-                text: "MyLinkedIn",
-                icon: <LinkedInIcon/>
-            }, 
-            Github:{
-                link: "https://github.com/VahidGhadiri",
-                text: "MyGithub",
-                icon: <GitHubIcon/>
-            },
-            Instagram:{
-                link:"https://www.instagram.com/_whyd_rf/", 
-                text:"MyInstagram", 
-                icon: <InstagramIcon/>
-            }
+  socials: {
+    LinkedIn: {
+      link: "https://www.linkedin.com/in/vahidghadiri/",
+      text: "MyLinkedIn",
+      icon: <LinkedInIcon />,
     },
+    Github: {
+      link: "https://github.com/VahidGhadiri",
+      text: "MyGithub",
+      icon: <GitHubIcon />,
+    },
+    Instagram: {
+      link: "https://www.instagram.com/_whyd_rf/",
+      text: "MyInstagram",
+      icon: <InstagramIcon />,
+    },
+  },
 
-    about: " I am a self-taught developer; I consider myself a motivated person, a lover of learning and a lover of teamwork; I am always indescribably eager to learn new contents, I am already fluent in HTML and CSS and familiar with JavaScript as well as React, also as a web developer I had a look at UI/UX  And I have read many books and articles in this regard. Certainly as a programmer, I pay special attention to things like data structure, algorithms, design pattern, and oop. Accurate learning of these things is one of my priorities...\n\n If I want to name one of the most important personality traits, I can point to a lot of perseverance, I will never stop trying and searching until I reach my goal. I do not hesitate to ask questions and I willingly teach others everything I know. The best way to learn is to teach. Hope to see",
+  about:
+    " I am a self-taught developer; I consider myself a motivated person, a lover of learning and a lover of teamwork; I am always indescribably eager to learn new contents, I am already fluent in HTML and CSS and familiar with JavaScript as well as React, also as a web developer I had a look at UI/UX  And I have read many books and articles in this regard. Certainly as a programmer, I pay special attention to things like data structure, algorithms, design pattern, and oop. Accurate learning of these things is one of my priorities...\n\n If I want to name one of the most important personality traits, I can point to a lot of perseverance, I will never stop trying and searching until I reach my goal. I do not hesitate to ask questions and I willingly teach others everything I know. The best way to learn is to teach. Hope to see",
 
-    design:[
-        {
-            title:"HTML", 
-            data:"Data 1", 
-            description: "Mastery",
-        },{
-            title:"CSS", 
-            data:"Data 2", 
-            description: "Mastery .",
-        },{
-            title:" SASS / SCSS", 
-            data:"Data 3", 
-            description: "Familiar",
-        },{
-            title:"Material-UI", 
-            data:"Responsive Design", 
-            description: "Fluent",
-        }
-        ]   ,
+  design: [
+    {
+      title: "HTML",
+      data: "Data 1",
+      description: "Mastery",
+    },
+    {
+      title: "CSS",
+      data: "Data 2",
+      description: "Mastery .",
+    },
+    {
+      title: " SASS / SCSS",
+      data: "Data 3",
+      description: "Familiar",
+    },
+    {
+      title: "Material-UI",
+      data: "Responsive Design",
+      description: "Fluent",
+    },
+  ],
 
-        
-        
-    programming:[
-        {
-            title:"Java Script", 
-            data: "Fluent", 
-            description: "Fluent"
-        },{
-            title:"ReactJs ", 
-            data:"Familiar" , 
-            description: "Fluent",
-        },{
-            title:"Redux", 
-            data:"Responsive Design", 
-            description: "Familiar",
-        },{
-            title:"ReactBootstrap", 
-            data:"Responsive Design", 
-            description: "Fluent",
-        }
-        ] ,
-        
-    axSkill:[
-            {
-                title: "UI/UX", 
-                description: "I was trying to understand the process of building a program from the beginning of creating an idea to the end of the path. Of course, I am familiar with applications such as Adobe XD and have the ability to build wireframes and prototypes.",
-                icon: <PaletteIcon/>
-            }, 
-            {
-                title: "Git", 
-                description: "The Git is very wide, but I know the ability to do routine tasks such as working with branches, Mergin them, pull, push so on. In fact, it's the routine that is required of any programmer.I will certainly add to my knowledge in this area.",
-                icon: <GitHubIcon/>
-            }, 
-            {
-                title: "Data Structure & Algorithm", 
-                description: "Personally, I am very interested in this part of programming,I have implemented some structures, and I am familiar with others. Along with this content, I am trying to learn Design pattern. Patterns such as builder, strategy, observer, single ton etc",
-                icon: <AccountTreeIcon/>
-            },
-        ],
-} 
+  programming: [
+    {
+      title: "Java Script",
+      data: "Fluent",
+      description: "Fluent",
+    },
+    {
+      title: "ReactJs ",
+      data: "Familiar",
+      description: "Fluent",
+    },
+    {
+      title: "Redux",
+      data: "Responsive Design",
+      description: "Familiar",
+    },
+    {
+      title: "ReactBootstrap",
+      data: "Responsive Design",
+      description: "Fluent",
+    },
+  ],
 
+  axSkill: [
+    {
+      title: "UI/UX",
+      description:
+        "I was trying to understand the process of building a program from the beginning of creating an idea to the end of the path. Of course, I am familiar with applications such as Adobe XD and have the ability to build wireframes and prototypes.",
+      icon: <PaletteIcon />,
+    },
+    {
+      title: "Git",
+      description:
+        "The Git is very wide, but I know the ability to do routine tasks such as working with branches, Mergin them, pull, push so on. In fact, it's the routine that is required of any programmer.I will certainly add to my knowledge in this area.",
+      icon: <GitHubIcon />,
+    },
+    {
+      title: "Data Structure & Algorithm",
+      description:
+        "Personally, I am very interested in this part of programming,I have implemented some structures, and I am familiar with others. Along with this content, I am trying to learn Design pattern. Patterns such as builder, strategy, observer, single ton and etc.",
+      icon: <AccountTreeIcon />,
+    },
+  ],
+
+  projects: [
+    {
+      tag: "UI / UX",
+      image:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+      title: "Project 1",
+      caption: "a short description ",
+      description: "this is my project and i wanna describe it",
+    },
+    {
+      tag: "UI / UX",
+      image:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+      title: "Project 1",
+      caption: "a short description ",
+      description: "this is my project and i wanna describe it",
+    },
+    {
+      tag: "HTML & CSS",
+      image:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+      title: "Project 1",
+      caption: "a short description ",
+      description: "this is my project and i wanna describe it",
+    },
+    {
+      tag: "JavaScript",
+      image:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+      title: "Project 1",
+      caption: "a short description ",
+      description: "this is my project and i wanna describe it",
+    },
+    {
+      tag: "React ",
+      image:
+        "https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip",
+      title: "Project 1",
+      caption: "a short description ", 
+      description: "this is my project and i wanna describe it",
+    },
+  ],
+};
